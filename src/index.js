9667aa387722ae40b5a74e976b15cc96fe813416
Gong Wang <gwa82@cs18026dr.canterbury.ac.nz>
2020-01-10 17:22:38 +1300

    joint rotation tested

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import * as THREE from 'three';
+import * as TMODULE from 'three/build/three.module.js';
 import utils from './utils.js';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const backgroundColor = 0xf1f1f1;
 const MODEL_PATH = 'models/stacy/stacy_lightweight.glb';
+let clock = new THREE.Clock();
+let neck, waist, head, hips, lShoulder, rShoulder, lArm, rArm, lHand, rHand,
+    lForeArm, rForeArm, lUpLeg, rUpLeg, lLeg, rLeg, lFoot, rFoot;
 
-
-var scene = new THREE.Scene();
+let scene = new THREE.Scene();
 scene.background = new THREE.Color(backgroundColor);
 scene.fog = new THREE.Fog(backgroundColor, 60, 100);
 
-var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
+let camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 30
 camera.position.x = 0;
 camera.position.y = -3;
 
-var renderer = new THREE.WebGLRenderer({
+let renderer = new THREE.WebGLRenderer({
     antialias: true
 });
 
@@ -56,16 +59,76 @@ floor.receiveShadow = true;
 floor.position.y = -11;
 scene.add(floor);
 
-var loader = new GLTFLoader();
+let loader = new GLTFLoader();
+let mixer = null;
 
 loader.load(
     MODEL_PATH,
     function(gltf) {
         // A lot is going to happen here
-        var model = gltf.scene;
+        let model = gltf.scene;
         let fileAnimations = gltf.animations;
 
+        model.traverse(o => {
+            if ( o.isMesh ) {
+                o.castShadow = true;
+                o.receiveShadow = true;
+            }
+
+            if ( o.isBone && o.name === 'mixamorigNeck' ) {
+                neck = o;
+            } else if ( o.isBone && o.name === 'mixamorigSpine' ) {
+                waist = o;
+            } else if ( o.isBone && o.name === 'mixamorigHead' ) {
+                head = o;
+            } else if ( o.isBone && o.name === 'mixamorigHips' ) {
+                hips = o;
+            } else if ( o.isBone && o.name === 'mixamorigLeftShoulder' ) {
+                lShoulder = o;
+            } else if ( o.isBone && o.name === 'mixamorigRightShoulder' ) {
+                rShoulder = o;
+            } else if ( o.isBone && o.name === 'mixamorigLeftArm' ) {
+                lArm = o;
+            } else if ( o.isBone && o.name === 'mixamorigRightArm' ) {
+                rArm = o;
+            } else if ( o.isBone && o.name === 'mixamorigLeftHand' ) {
+                lHand = o;
+            } else if ( o.isBone && o.name === 'mixamorigRightHand' ) {
+                rHand = o;
+            } else if ( o.isBone && o.name === 'mixamorigLeftForeArm' ) {
+                lForeArm = o;
+            } else if ( o.isBone && o.name === 'mixamorigRightForeArm' ) {
+                rForeArm = o;
+            } else if ( o.isBone && o.name === 'mixamorigLeftUpLeg' ) {
+                lUpLeg = o;
+            } else if ( o.isBone && o.name === 'mixamorigRightUpLeg' ) {
+                rUpLeg = o;
+            } else if ( o.isBone && o.name === 'mixamorigLeftLeg' ) {
+                lLeg = o;
+            } else if ( o.isBone && o.name === 'mixamorigRightLeg' ) {
+                rLeg = o;
+            } else if ( o.isBone && o.name === 'mixamorigLeftFoot' ) {
+                lFoot = o;
+            } else if ( o.isBone && o.name === 'mixamorigRightFoot' ) {
+                rFoot = o;
+            }
+
+            // Investigation process
+            // if ( o.isBone ) {
+            //     console.log(o.name);
+            // }
+        });
+
+        model.scale.set(7, 7, 7);
+        model.position.y = -11;
+
         scene.add(model);
+
+        renderTexture(model);
+        // renderAnimation(model, fileAnimations);
+
+        moveJoint(neck);
+
     },
     undefined, // We don't need this function
     function(error) {
@@ -73,10 +136,56 @@ loader.load(
     }
 );
 
+function renderTexture( model ) {
+    let txt_loader = new TMODULE.TextureLoader;
+    txt_loader.load(
+        'models/stacy/stacy.jpg',
+        function ( stacy_txt ) {
+            stacy_txt.flipY = false; // we flip the texture so that its the right way up
+
+            const stacy_mtl = new THREE.MeshPhongMaterial({
+                map: stacy_txt,
+                color: 0xffffff,
+                skinning: true
+            });
+
+            model.traverse(o => {
+                if (o.isMesh) {
+                    o.castShadow = true;
+                    o.receiveShadow = true;
+                    o.material = stacy_mtl; // Add this line
+                }
+            });
+        },
+        undefined,
+        function ( err ) {
+            console.log(err);
+        }
+    );
+}
+
+function renderAnimation( model, fileAnimations ) {
+    mixer = new THREE.AnimationMixer(model);
+    let idleAnim = THREE.AnimationClip.findByName(fileAnimations, "idle");
+
+    let idle = mixer.clipAction(idleAnim);
+    idle.play();
+}
+
+function moveJoint(joint) {
+    console.log(joint)
+    joint.rotation.y = THREE.Math.degToRad(30);
+    joint.rotation.x = THREE.Math.degToRad(30);
+}
+
 function animate() {
 	requestAnimationFrame( animate );
 
-    if (utils.resizeRendererToDisplaySize(renderer)) {
+    if ( mixer ) {
+        mixer.update(clock.getDelta());
+    }
+
+    if ( utils.resizeRendererToDisplaySize(renderer) ) {
         const canvas = renderer.domElement;
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
